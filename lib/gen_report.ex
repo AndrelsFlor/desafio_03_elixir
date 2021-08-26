4639063bcaf07f05a437268da2e7189e88287ad5
André Luís de Sousa Flor <andreluisflor@live.com>
2021-08-25 22:16:53 -0300

    returning all hours

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,62 @@
 defmodule GenReport do
+  @months [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ]
 
-  def build do
-    # TO DO
+  @years [2016, 2017, 2018, 2019, 2020]
+
+  @names [
+    "Cleiton",
+    "Daniele",
+    "Danilo",
+    "Diego",
+    "Giuliano",
+    "Jakeliny",
+    "Joseph",
+    "Mayk",
+    "Rafael",
+    "Vinicius"
+  ]
+  alias GenReport.Parser
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    # |> Enum.map(& &1)
+    |> Enum.reduce(zeroed_reports(), fn line, report -> sum_hours(line, report) end)
+  end
+
+  defp sum_hours(line, report) do
+    [name, hours, _month_day, _month_name, _year] = line
+    %{"all_hours" => all_hours} = report
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+    %{"all_hours" => all_hours}
+  end
+
+  defp zeroed_reports() do
+    all_hours = Enum.into(@names, %{}, &{&1, 0})
+
+    hours_per_month =
+      Enum.into(@names, %{}, fn name -> {name, Enum.into(@months, %{}, &{&1, 0})} end)
+
+    hours_per_year =
+      Enum.into(@names, %{}, fn name -> {name, Enum.into(@years, %{}, &{&1, 0})} end)
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
---
+++ lib/parser.ex
@@ -0,0 +1,39 @@
+defmodule GenReport.Parser do
+  @months %{
+    "1" => "Janeiro",
+    "2" => "Fevereiro",
+    "3" => "Março",
+    "4" => "Abril",
+    "5" => "Maio",
+    "6" => "Junho",
+    "7" => "Julho",
+    "8" => "Agosto",
+    "9" => "Setembro",
+    "10" => "Outubro",
+    "11" => "Novembro",
+    "12" => "Dezembro"
+  }
+
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(4, &String.to_integer/1)
+    |> get_month_name()
+
+    # |> generate_line_map()
+  end
+
+  defp get_month_name(line) do
+    line
+    |> List.update_at(3, &@months[&1])
+  end
+end
